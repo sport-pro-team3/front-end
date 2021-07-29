ab4448f1e20b70a46f9f008d25f8441d15fe33e2
nur4aa <38027647+nur4aa@users.noreply.github.com>
2021-07-29 11:28:57 +0600

    add auto slider

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,12 @@
 import "./Intro.css";
-import { MainPart } from "./../main/Main";
-import { Carousel } from "react-bootstrap";
+import { MainPart } from "../main/Main";
+import Carousel from "react-bootstrap/Carousel";
 import introPhoto from "./intro-photo.png";
 
 export const Intro = () => {
   return (
     <>
-      <Slider></Slider>
+      <Slider className="intro__slider"></Slider>
       <div className="intro__title">
         <h2 className="intro__title__left">all in sport kyrgyz republic</h2>
         <h2 className="intro__title__right">
@@ -24,6 +24,33 @@ export const Intro = () => {
 
 function Slider() {
   return (
+    <Carousel variant="dark">
+      <Carousel.Item>
+        <img className="d-block w-100" src={introPhoto} alt="First slide" />
+        <Carousel.Caption>
+          {" "}
+          <h3>Чемпионат Азии по боксу</h3>
+          <p>12.05.2021 - 18.05.2021</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={introPhoto} alt="Second slide" />
+        <Carousel.Caption>
+          {" "}
+          <h3>Чемпионат Азии по боксу</h3>
+          <p>12.05.2021 - 18.05.2021</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className="d-block w-100" src={introPhoto} alt="Third slide" />
+        <Carousel.Caption>
+          <h3>Чемпионат Азии по боксу</h3>
+          <p>12.05.2021 - 18.05.2021</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+
+    /* 
     <Carousel>
       <Carousel.Item>
         <img src={introPhoto} alt="" className="d-block w-100 h-500" />
@@ -32,6 +59,6 @@ function Slider() {
           <p>12.05.2021 - 18.05.2021</p>
         </Carousel.Caption>
       </Carousel.Item>
-    </Carousel>
+    </Carousel>*/
   );
 }
