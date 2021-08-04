3a831b4c2f4d88000eb7e2d7a32d3e94d1b4b01f
nur4aa <38027647+nur4aa@users.noreply.github.com>
2021-08-04 14:08:15 +0600

    filter for news

---
+++ src/pages/News/Filter.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Select from "react-select";
+import { useEffect, useState } from "react";
+import { axios } from "axios";
+
+function Filter() {
+  return (
+    <div>
+      <Select></Select>
+    </div>
+  );
+}
+
+export default Filter;
